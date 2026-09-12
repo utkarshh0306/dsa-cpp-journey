c3850941def432dd787618fc0e54939c293aad35
Utkarsh <utkarshtjadhav06@gmail.com>
2026-09-12 11:44:14 +0530

    Add Traversal method Preorder Traversal

---
+++ 33 Binary Tree/02PreorderTraversal.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> nodes){
+    idx++;
+    if(nodes[idx] == -1){
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);
+
+    currNode->left = buildTree(nodes);
+    currNode->right = buildTree(nodes);
+
+    return currNode;
+}
+
+void preorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    cout<<root->data<<" ";
+
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main(){
+    vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+    Node* root = buildTree(nodes);
+    cout<<"root : "<<root->data<<endl;
+    cout<<"Preorder List is: ";
+    preorder(root);
+
+    return 0;
+}
