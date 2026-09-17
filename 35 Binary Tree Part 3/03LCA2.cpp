7c28c9bd8fa9ca34d1bc85b00bae221dd9fc1f55
Utkarsh <utkarshtjadhav06@gmail.com>
2026-09-17 08:47:26 +0530

    Add solution of lowest common ancestor approach 2

---
+++ 35 Binary Tree Part 3/03LCA2.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> nodes){
+    idx++;
+    if(nodes[idx] == -1){
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);
+
+    currNode->left = buildTree(nodes);
+    currNode->right = buildTree(nodes);
+
+    return currNode;
+}
+
+Node* LCA(Node* root , int n1 , int n2){
+    if(root == NULL){
+        return NULL;
+    }
+
+    if(root->data == n1 || root->data == n2){
+        return root;
+    }
+
+    Node* leftLCA = LCA(root->left , n1 , n2);
+    Node* rightLCA = LCA(root->right , n1 , n2);
+
+    if(leftLCA != NULL && rightLCA != NULL){
+        return root;
+    }
+
+    return leftLCA == NULL ? rightLCA : leftLCA;
+}
+
+int main(){
+    vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+    Node* root = buildTree(nodes);
+    cout<<LCA(root , 4 , 5)->data;
+
+    return 0;
+}
